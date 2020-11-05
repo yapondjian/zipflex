1194cf654a41e7849e20358f4d226a540f9c62dc
Yapondjian <junioryapondjian@gmail.com>
2020-11-05 01:18:57 -0300

    [Header] - QA

--- src/assets/common/js/modules/geral/menu.js
+++ src/assets/common/js/modules/geral/menu.js
@@ -1,29 +1,21 @@
 const Methods = {
     init() {
-        Methods.getCollection();
+        Methods.openMenu();
+        Methods.search();
     },
 
-    getItem($skulist) {
-        const $numWish = 1;
-        const $shelfIDHeader = "44f537fa-5757-464a-8221-b8d5eaecd62d";
-
-        $.ajax(`/buscapagina?fq=productId:${$skulist}&PS=${$numWish}&sl=${$shelfIDHeader}&cc=100&sm=0&PageNumber=${$numWish}`, {
-            async: !1
-        }).done(function($response) {
-            $(`.j-link__items--sub-product[data-cluster-id="${$skulist}"]`).html($response);
-        }).fail(function(err) {
-            console.log(err);
-        });
-
+    openMenu() {
+        $('.js--menu, .y-header__menu .y-close').live('click', function(){
+            $('body').toggleClass('no--scroll');
+            $('.y-header__menu').toggleClass('is--active')
+        })
     },
 
-    getCollection() {
-        const $menuShelf = document.querySelectorAll('.j-link__items--sub-product');
-
-        $menuShelf.forEach($item => {
-            let $skuId = $item.dataset.clusterId;
-            Methods.getItem($skuId);
-        });
+    search() {
+        $('.js--search').live('click', function(){
+            $('body').toggleClass('no--scroll');
+            $('.js--search-content').toggleClass('is--active');
+        })
     }
 };
 
