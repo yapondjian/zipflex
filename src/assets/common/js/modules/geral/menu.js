54fd89baaaecda4e53651b4a19ba8903e884a5a9
Yapondjian <junioryapondjian@gmail.com>
2020-11-25 00:45:51 -0300

    [Mobile/Home] - Correções #1

--- src/assets/common/js/modules/geral/menu.js
+++ src/assets/common/js/modules/geral/menu.js
@@ -7,14 +7,18 @@ const Methods = {
     openMenu() {
         $('.js--menu').live('click', function(){
             $('.y-header__menu').toggleClass('is--active');
-        })
+        });
+
+        $('.y-header__menu-list li:first-child').live('click', function(){
+            $(this).toggleClass('is--active');
+        });
     },
 
     search() {
         $('.js--search, .js--search-content .y-close').live('click', function(){
             $('body').toggleClass('no--scroll');
             $('.js--search-content').toggleClass('is--active');
-        })
+        });
     }
 };
 
